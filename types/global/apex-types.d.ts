7b21f54a358286a5aa6099a509309a08f7f66244
Sean Keane <seankeane785@gmail.com>
2025-09-15 15:00:42 +0100

    chore: add apex type modules

---
+++ types/global/apex-types.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="./contracts.d.ts" />
+export type ApexStrategyId = PrismApex.StrategyId;
+export type ApexGuardrailReason = PrismApex.GuardrailReason;
---
+++ types/global/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="./contracts.d.ts" />
+/// <reference path="./env.d.ts" />
+/// <reference path="./json.d.ts" />
+/// <reference path="./vitest.d.ts" />
