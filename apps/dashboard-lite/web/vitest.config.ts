d4091d39a6a227f13f9ddb19de759b28ce9b767d
Sean Keane <seankeane785@gmail.com>
2025-09-15 08:54:38 +0100

    chore: unify tsconfig path management

--- apps/dashboard-lite/web/vitest.config.ts
+++ apps/dashboard-lite/web/vitest.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig } from 'vitest/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: { include: ['test/**/*.test.tsx'], watch: false, passWithNoTests: false },
 });
