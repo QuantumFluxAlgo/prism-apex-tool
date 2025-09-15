a28240a9878ade97eae10f1beabb653a9e1d0d49
Sean Keane <seankeane785@gmail.com>
2025-09-15 08:54:38 +0100

    chore: unify tsconfig path management

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vitest/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: {
     globals: true,
     include: ['**/__tests__/**/*.spec.ts', '**/*.spec.ts'],
